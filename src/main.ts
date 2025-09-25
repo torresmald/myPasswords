19bebd991f603251e9ffc8027ce6c3f1d8f31d9f
Jonathan Torres <jonathan.torresmald@gmail.com>
2025-09-25 15:57:16 +0200

    wip: delete endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+inject();
+
+injectSpeedInsights();
